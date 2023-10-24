b38388e4c1b5a0135aa26daef8c95e0674560621
Soni Kumari <76401953+sonikumari231@users.noreply.github.com>
2023-10-24 17:18:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Resturant from "./components/Basics/Resturant";
+//import UseState from "./component/hooks/useState";
+//import UseEffect from "./components/hooks/useEffect";
+//import UseReducer from "./components/hooks/useReducer";
+
+const App = () => {
+  return <Resturant />;
+};
+
+export default App;
